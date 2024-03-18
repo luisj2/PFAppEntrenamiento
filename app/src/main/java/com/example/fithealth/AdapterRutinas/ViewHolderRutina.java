05653ab20fbd6a5c1302a492e919bd6befc47f3e
luisj2 <luisjesusrodriguez02@gmail.com>
2024-03-18 19:38:35 +0100

    social_barraNavegacion

--- app/src/main/java/com/example/fithealth/AdapterRutinas/ViewHolderRutina.java
+++ app/src/main/java/com/example/fithealth/AdapterRutinas/ViewHolderRutina.java
@@ -1,6 +1,8 @@
 package com.example.fithealth.AdapterRutinas;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,9 +13,21 @@ import com.example.fithealth.R;
 public class ViewHolderRutina extends RecyclerView.ViewHolder {
 
     private TextView tvNombreRutina;
+    private Button button;
     public ViewHolderRutina(@NonNull View itemView) {
         super(itemView);
         tvNombreRutina = itemView.findViewById(R.id.tvRutina);
+        button = itemView.findViewById(R.id.btnActivityMostrarRutinas);
+        funcionalidadButton();
+    }
+
+    private void funcionalidadButton() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("informacionBoton","La informacion del este item es "+tvNombreRutina.getText().toString());
+            }
+        });
     }
 
     public void cambiarTextViewRutina(String nombreRutina){
